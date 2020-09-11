6671cdcd9440c8d3aa20def86e94ad415131ff32
Lyneca <atraguliauniliantauthr@gmail.com>
2020-09-12 03:56:20 +1000

    Lots of stuff, Effects are being a pain

---
+++ DaggerMerge.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using ThunderRoad;
+
+namespace DaggerSpell {
+    class DaggerMerge : SpellMergeData {
+        public int numDaggers = 3;
+        private bool isActive;
+        List<Item> daggers = new List<Item>();
+        ItemPhysic daggerBase;
+        public override void OnCatalogRefresh() {
+            base.OnCatalogRefresh();
+            daggerBase = Catalog.GetData<ItemPhysic>("DaggerCommon", true);
+            daggers = new List<Item>();
+        }
+
+        private Vector3 GetVelocity(GameObject obj) {
+            return obj.GetComponent<Rigidbody>().velocity;
+        }
+
+        // private void RenderLines(GameObject bone) {
+        //     LineRenderer lineRenderer = bone.GetComponent<LineRenderer>() ?? bone.AddComponent<LineRenderer>();
+        //     lineRenderer.positionCount = 2;
+        //     lineRenderer.startWidth = 0.01f;
+        //     lineRenderer.endWidth = 0.01f;
+        //     lineRenderer.startColor = Color.green;
+        //     lineRenderer.endColor = Color.green;
+        //     lineRenderer.SetPosition(0, bone.transform.position);
+        //     lineRenderer.SetPosition(1, bone.transform.right * -10.0f + bone.transform.position);
+        // }
+
+        public override void Merge(bool active) {
+            base.Merge(active);
+            // RenderLines(Creature.player.animator.GetBoneTransform(HumanBodyBones.LeftHand).gameObject);
+            // RenderLines(Creature.player.animator.GetBoneTransform(HumanBodyBones.RightHand).gameObject);
+            if (active && !isActive) {
+                isActive = true;
+                for (int i = 0; i < 3; i++) {
+                    Item dagger = spawnDagger();
+                    daggers.Add(dagger);
+                    Vector3 target = Vector3.Lerp(
+                        Creature.player.animator.GetBoneTransform(HumanBodyBones.LeftHand).position,
+                        Creature.player.animator.GetBoneTransform(HumanBodyBones.RightHand).position,
+                        (i + 1.0f) / 4.0f
+                     );
+                    dagger.transform.position = target;
+                    dagger.transform.rotation = GetHandsPointingQuaternion() * Quaternion.Euler(0, -90, 0);
+                }
+            } else {
+                if (currentCharge == 1) {
+                    foreach (Item dagger in daggers) {
+                        releaseDagger(dagger);
+                        dagger.GetComponent<Rigidbody>().AddForce(GetHandsPointingQuaternion() * Vector3.forward * 20.0f, ForceMode.Impulse);
+                        dagger.Throw();
+                    }
+                } else {
+                    foreach (Item dagger in daggers) {
+                        dagger.Despawn();
+                    }
+                }
+                daggers.Clear();
+                isActive = false;
+            }
+        }
+
+        private Item spawnDagger() {
+            Item dagger = daggerBase.Spawn();
+            dagger.GetComponent<Rigidbody>().isKinematic = true;
+            return dagger;
+        }
+
+        private void releaseDagger(Item dagger) {
+            dagger.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        private Vector3 GetHandCenterPoint() {
+            return Vector3.Lerp(
+                Creature.player.body.handLeft.transform.position,
+                Creature.player.body.handRight.transform.position,
+                0.5f
+            );
+        }
+
+        private Quaternion GetHandsPointingQuaternion() {
+            return Quaternion.Slerp(
+                Quaternion.LookRotation(Creature.player.animator.GetBoneTransform(HumanBodyBones.RightHand).transform.right * -1.0f),
+                Quaternion.LookRotation(Creature.player.animator.GetBoneTransform(HumanBodyBones.LeftHand).transform.right * -1.0f),
+                0.5f
+            );
+        }
+
+        private void PositionDaggers() {
+            int i = 0;
+            foreach (Item dagger in daggers) {
+                if (dagger == null)
+                    continue;
+                dagger.transform.localScale = Vector3.one * currentCharge;
+                Vector3 target = Vector3.Lerp(
+                    Creature.player.animator.GetBoneTransform(HumanBodyBones.LeftHand).position,
+                    Creature.player.animator.GetBoneTransform(HumanBodyBones.RightHand).position,
+                    ++i / 4.0f
+                 );
+                dagger.transform.position = Vector3.Lerp(dagger.transform.position, target, Time.deltaTime * 10.0f);
+                dagger.transform.rotation = Quaternion.Slerp(dagger.transform.rotation, GetHandsPointingQuaternion() * Quaternion.Euler(0, -90, 0), Time.deltaTime * 10.0f);
+            }
+        }
+
+        public override void Update() {
+            base.Update();
+            PositionDaggers();
+        }
+    }
+}
--- DaggerSpell.cs
+++ DaggerSpell.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using ThunderRoad;
+using System.ComponentModel;
 
 namespace DaggerSpell {
     public class DaggerSpell : SpellCastCharge {
@@ -12,17 +13,31 @@ namespace DaggerSpell {
         private bool daggerActive;
         private ItemPhysic daggerBase;
         private Item summonedDagger;
+        private EffectData throwEffect;
+        private GameObject blackHolePrefab;
+        private GameObject blackHole;
+        EffectInstance throwEffectInstance;
         public override void OnCatalogRefresh() {
             base.OnCatalogRefresh();
-            AssetBundle itemsBundle = AssetBundle.GetAllLoadedAssetBundles().Where(x => x.name.Contains("items")).First();
-            // itemsBundle.LoadAsset<GameObject>("assets/private/bas-items/melee/daggers/wpn_da02_common.prefab");
+            AssetBundle assetBundle = AssetBundle.GetAllLoadedAssetBundles().Where(bundle => bundle.name.Contains("blackhole")).First();
+            blackHolePrefab = assetBundle.LoadAsset<GameObject>("BlackHole.prefab");
             daggerBase = Catalog.GetData<ItemPhysic>("DaggerCommon", true);
+            throwEffect = Catalog.GetData<EffectData>("SpellTelekinesisPush");
+        }
+
+        public override void Init() {
+            base.Init();
+        }
+
+        private float GetBlackHoleIntensityFromCharge() {
+            return (float)Math.Max(Math.Min(Math.Round(Math.Sin(currentCharge * Math.PI) * currentCharge * 1.5f, 3), 1.0f), 0.0f);
         }
 
         public override void UpdateCaster() {
             base.UpdateCaster();
             if (daggerActive) {
                 summonedDagger.transform.localScale = Vector3.one * currentCharge;
+                blackHole.transform.localScale = Vector3.one * GetBlackHoleIntensityFromCharge() * 0.2f;
                 summonedDagger.transform.position = Vector3.Lerp(summonedDagger.transform.position, GetTargetPosition(), Time.deltaTime * 10);
                 switch (spellCaster.bodyHand.side) {
                     case Side.Left:
@@ -32,6 +47,8 @@ namespace DaggerSpell {
                         summonedDagger.transform.rotation = Quaternion.Lerp(summonedDagger.transform.rotation, spellCaster.bodyHand.transform.rotation, Time.deltaTime * 10);
                         break;
                 }
+                blackHole.transform.position = summonedDagger.transform.position;
+                blackHole.transform.rotation = summonedDagger.transform.rotation;
                 if (PlayerControl.GetHand(spellCaster.bodyHand.side).gripPressed && spellCaster.bodyHand.interactor.grabbedHandle == null && currentCharge == 1) {
                     EnableDagger();
                     switch (spellCaster.bodyHand.side) {
@@ -57,8 +74,21 @@ namespace DaggerSpell {
 
         public void EnableDagger() {
             Rigidbody rb = summonedDagger.GetComponent<Rigidbody>();
+            UnityEngine.Object.Destroy(blackHole);
             rb.isKinematic = false;
         }
+
+
+
+        private Bounds GetMaxBounds(GameObject g) {
+            var b = new Bounds(g.transform.position, Vector3.zero);
+            foreach (Renderer r in g.GetComponentsInChildren<Renderer>()) {
+                b.Encapsulate(r.bounds);
+            }
+            return b;
+        }
+
+
 
         public override void Fire(bool active) {
             base.Fire(active);
@@ -66,27 +96,45 @@ namespace DaggerSpell {
                 if (!daggerActive) {
                     daggerActive = true;
                     summonedDagger = daggerBase.Spawn(true, null);
-                    Rigidbody rb = summonedDagger.GetComponent<Rigidbody>();
-                    rb.isKinematic = true;
+                    summonedDagger.GetComponent<Rigidbody>().isKinematic = true;
                     summonedDagger.transform.position = GetTargetPosition();
+                    blackHole = UnityEngine.Object.Instantiate(blackHolePrefab, summonedDagger.transform.position, summonedDagger.transform.rotation);
+                    blackHole.transform.localScale = Vector3.one * 0.25f;
+                    blackHole.transform.localPosition = Vector3.zero;
+                    blackHole.GetComponent<Renderer>().material.SetFloat("HoleSize", 0);
+                    blackHole.GetComponent<Renderer>().material.SetFloat("DistortionStrength", 0);
                 }
             } else {
+                EnableDagger();
                 if (currentCharge != 1) {
                     summonedDagger.Despawn();
+                    if (summonedDagger != null)
+                        UnityEngine.Object.Destroy(summonedDagger);
                 }
                 daggerActive = false;
-                EnableDagger();
             }
         }
 
         public override void Throw(Vector3 velocity) {
-            base.Throw(velocity);
-            EnableDagger();
-            Rigidbody rb = summonedDagger.GetComponent<Rigidbody>();
-            summonedDagger.transform.rotation = Quaternion.LookRotation(velocity) * Quaternion.LookRotation(Vector3.left);
-            rb.AddForce(velocity * 5, ForceMode.Impulse);
-            summonedDagger.Throw();
-            daggerActive = false;
+            if (daggerActive) {
+                base.Throw(velocity);
+                EnableDagger();
+                Rigidbody rb = summonedDagger.GetComponent<Rigidbody>();
+                summonedDagger.transform.rotation = Quaternion.LookRotation(velocity) * Quaternion.LookRotation(Vector3.left);
+                rb.AddForce(velocity * 5, ForceMode.Impulse);
+                //throwEffectInstance = throwEffect.Spawn(Creature.player.body.GetHand(spellCaster.bodyHand.side).transform);
+                throwEffectInstance = throwEffect.Spawn(Vector3.zero, Quaternion.identity);
+                throwEffectInstance.Play();
+                throwEffectInstance.SetIntensity(1f);
+                //foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(throwEffectInstance)) {
+                //    string name = descriptor.Name;
+                //    object value = descriptor.GetValue(throwEffectInstance);
+                //    Debug.Log($"{name}={value}");
+                //}
+                //throwEffectInstance.onEffectFinished += (effect) => { effect.Stop(); effect.Despawn(); };
+                summonedDagger.Throw();
+                daggerActive = false;
+            }
         }
     }
 }
